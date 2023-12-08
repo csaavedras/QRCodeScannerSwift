dbb769d8070bc38f634c9e2d31328e7500475afe
Camilo Saavedra <ci.saavedras@gmail.com>
2023-12-07 21:53:50 -0300

    Initial Commit

---
+++ QRCodeScanner/QRCodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeScannerApp.swift
+//  QRCodeScanner
+//
+//  Created by Camilo Saavedra Salas on 07-12-23.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
